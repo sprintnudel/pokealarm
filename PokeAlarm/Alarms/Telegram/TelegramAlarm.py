60ac7ea4154b3155739beda159e6b302eaa267cd
Sprintnudel <sprintnudel@gmail.com>
2020-01-24 17:54:12 +0100

    telegram: send map before message

--- PokeAlarm/Alarms/Telegram/TelegramAlarm.py
+++ PokeAlarm/Alarms/Telegram/TelegramAlarm.py
@@ -233,13 +233,13 @@ class TelegramAlarm(Alarm):
                 bot_token, chat_id, lat, lng, message, max_attempts)
             return  # Don't send message or map
 
-        # Send Message
-        self.send_message(bot_token, chat_id, replace(message, dts),
-                          web_preview=alert.web_preview)
-
         # Send Map
         if alert.map:
             self.send_location(bot_token, chat_id, lat, lng, max_attempts)
+
+        # Send Message
+        self.send_message(bot_token, chat_id, replace(message, dts),
+                          web_preview=alert.web_preview)
 
     # Trigger an alert based on Pokemon info
     def pokemon_alert(self, mon_dts):
